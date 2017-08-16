9136f4f374377dddd5a5742e6082ca032c5a40ed
mdu74 <mdu74.C@gmail.com>
2017-08-16 15:03:30 +0200

    first fizzbuzz commit

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+function IsFizzBuzz(input) {
+    if (DivisibleByThreeAndFive(input))
+        return 'fizzbuzz';
+    if (DivisibleByThree(input))
+        return 'fizz';
+    if (DivisibleByFive(input))
+        return 'buzz';
+    return input;
+}
+
+function DivisibleByThree(input) {
+    return input % 3 == 0;
+}
+
+function DivisibleByFive(input) {
+    return input % 5 == 0;
+}
+
+function DivisibleByThreeAndFive(input) {
+    return DivisibleByThree(input) && DivisibleByFive(input);
+}
---
+++ fizzbuzz.spec.js
@@ -0,0 +1,93 @@
+describe("fizzbuzz", function() {
+    it("Given number 1 should return 1", function() {
+        var numbersNotDivisibleByBothThreeAndFive = [1, 2, 4, 7];
+        numbersNotDivisibleByBothThreeAndFive.forEach(function(numberNotDivisbleByBothThreeAndFive) {
+            //---------------Set up test pack-------------------
+            var input = numberNotDivisbleByBothThreeAndFive;
+            //---------------Execute Test ----------------------
+            var fizzBuzz = IsFizzBuzz(input);
+            //---------------Test Result -----------------------
+            expect(fizzBuzz).toBe(numberNotDivisbleByBothThreeAndFive);
+        });
+    });
+    it("Given number 2 should return 2", function() {
+        //---------------Set up test pack-------------------
+        var input = 2;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe(2);
+    });
+    it("Given number 3 should return fizz", function() {
+        //---------------Set up test pack-------------------
+        var input = 3;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizz');
+    });
+    it("Given number 5 should return buzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 5;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('buzz');
+    });
+    it("Given number 6 should return fizz", function() {
+        //---------------Set up test pack-------------------
+        var input = 6;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizz');
+    });
+    it("Given number 9 should return fizz", function() {
+        //---------------Set up test pack-------------------
+        var input = 9;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizz');
+    });
+    it("Given number 10 should return buzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 10;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('buzz');
+    });
+    it("Given number 15 should return fizzbuzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 15;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizzbuzz');
+    });
+    it("Given number 20 should return buzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 20;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('buzz');
+    });
+    it("Given number 30 should return buzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 30;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizzbuzz');
+    });
+    it("Given number 75 should return buzz", function() {
+        //---------------Set up test pack-------------------
+        var input = 75;
+        //---------------Execute Test ----------------------
+        var fizzBuzz = IsFizzBuzz(input);
+        //---------------Test Result -----------------------
+        expect(fizzBuzz).toBe('fizzbuzz');
+    });
+});
